ec36c8b71f51b10a2844764954eae6eb4b15c96d
Alec Ross <alexander.w.rosss@gmail.com>
2023-03-30 00:39:08 -0400

    add dump and seed commands

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# PostgreSQL Backup and Restore Script
-`pg_backup_and_restore` is a command-line utility that simplifies the process of backing up a PostgreSQL database from a staging server and restoring it to a local database. This utility is particularly useful for developers who need to work with an up-to-date copy of a staging database on their local machines.
+# Database Backup and Restore
+
+This script allows you to backup and restore PostgreSQL databases using the pg_dump command. It supports backing up both remote and local databases and provides a command-line interface for specifying various options, such as host, port, username, and database.
 
 ## Requirements
 
-- Rust 1.31 or later
-- PostgreSQL client tools (`pg_dump`, `createdb`, and `pg_restore`) installed and available in your system's `PATH`
+- Rust programming language installed
+- PostgreSQL installed with `pg_dump` available in the system path
+- Clap and Chrono Rust crates
 
 ## Installation
 
@@ -32,26 +34,56 @@ cargo build --release
 cp target/release/pg_backup_and_restore /usr/local/bin/
 ```
 
-5. Usage
+## Usage
 
 ```bash
-pg_backup_and_restore -h <host> -p <port> -U <username> -s <staging_db> -u <local_username> -l <local_db>
+USAGE: 
+    pg_backup_and_restore [SUBCOMMAND]
+
+SUBCOMMANDS
+    dump: Backup a database
+    OPTIONS
+        -o, --output-file-location OUTPUT_FILE_LOCATION: Location to save the backup file (default: .)
+    
+    seed: Seed a local database
+        
+OPTIONS
+-h, --host HOST: Sets the host to connect to (required)
+-p, --port PORT: Sets the port to connect to (default: 5432)
+-u, --username USERNAME: Sets the username to connect with (required)
+-d, --database DATABASE: Sets the database to connect to (required)
 ```
 
-Replace the following placeholders with the appropriate values:
+Then, you can run the compiled
 
-- `<host>`: The staging database host
-- `<port>`: The staging database port (default is 5432)
-- `<username>`: The staging database username
-- `<staging_db>`: The staging database name
-- `<local_username>`: The local database username
-- `<local_db>`: The local database name
-The script will prompt you to enter the password for the staging database user.
+## Examples
 
-Example:
+To backup a database:
 
 ```bash
-pg_backup_and_restore -h staging.example.com -p 5432 -U myuser -s my_staging_db -u mylocaluser -l my_local_db
+$ pg_backup_and_restore dump -h <host> -u <username> -d <database>
 ```
 
-This command will backup the `my_staging_db` database from the staging server and restore it to the `my_local_db` local database.
+To seed a database:
+
+```bash
+$ pg_backup_and_restore seed -h localhost -u myuser -d mydatabase
+```
+
+To seed a database using a backup file from a custom output file location:
+
+```bash
+$ pg_backup_and_restore seed -h localhost -u myuser -d mydatabase -o /path/to/backups
+```
+
+## How It Works
+
+The application uses the Clap crate to define the command-line interface and parse the provided arguments. Based on the provided subcommand (`dump` or `seed`), the application will either create a backup dump of a database or seed a database using a backup file.
+
+When creating a backup dump, the application generates a backup filename with the current timestamp and the specified output file location. Then, it runs the `pg_dump` command with the provided options and saves the backup file to the specified location.
+
+When seeding a database, the application scans the specified directory (or `/tmp` by default) for the most recent `.sql` backup file. If it finds a suitable backup file, it runs the `psql` command with the provided options and seeds the database using the backup file. If no suitable backup file is found, it prints a message and exits.
+
+## Limitations
+
+This application currently only supports PostgreSQL databases and relies on the `pg_dum`p and `psql` commands being available in the system path
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,170 @@
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
 use std::process::Command;
+use std::fs;
+use std::path::Path;
 use chrono;
 
 fn main() {
-    let matches = App::new("Database Backup and Restore")
+    let app = App::new("Database Backup and Restore")
         .version("0.1.0")
         .author("Alec Ross")
-        .about("Backup and restore PostgreSQL databases")
-        .arg(
-            Arg::new("remote_host")
-            .short('h')
-            .long("remote-host")
-            .value_name("REMOTE_HOST")
+        .about("Backup and restore PostgreSQL databases");
+
+    let common_args = vec![
+        Arg::new("host")
+            .short('H')
+            .long("host")
+            .value_name("HOST")
             .help("Sets the host to connect to")
-            .required(true)
-        )
-        .arg(
-            Arg::new("remote_port")
+            .required(true),
+
+        Arg::new("port")
             .short('p')
-            .long("remote-port")
-            .value_name("REMOTE_PORT")
-            .help("Sets the remote port to connect to")
-            .default_value("5432")
-        )
-        .arg(
-            Arg::new("remote_username")
+            .long("port")
+            .value_name("PORT")
+            .help("Sets the port to connect to")
+            .default_value("5432"),
+
+        Arg::new("username")
             .short('u')
-            .long("remote-username")
-            .value_name("REMOTE_USERNAME")
-            .help("Remote username to connect with")
-            .required(true)
-        )
-        .arg(
-            Arg::new("remote_db")
+            .long("username")
+            .value_name("USERNAME")
+            .help("Username to connect with")
+            .required(true),
+
+        Arg::new("database")
             .short('d')
-            .long("remote-db")
-            .value_name("REMOTE_DB")
-            .help("Remote database to connect to")
-            .required(true)
-        )
-        .get_matches();
+            .long("database")
+            .value_name("DATABASE")
+            .help("Database to connect to")
+            .required(true),
+    ];
 
-    let remote_host = matches.value_of("remote_host").unwrap();
-    let remote_port = matches.value_of("remote_port").unwrap();
-    let remote_username = matches.value_of("remote_username").unwrap();
-    let remote_db = matches.value_of("remote_db").unwrap();
+    let dump = SubCommand::with_name("dump")
+        .about("Backup a dump database")
+        .args(
+            &[
+                Arg::new("output_file_location")
+                    .short('o')
+                    .long("output-file-location")
+                    .value_name("OUTPUT_FILE_LOCATION")
+                    .help("Location to save the backup file")
+                    .default_value("."),
+            ]
+            .iter()
+            .chain(common_args.iter())
+            .cloned()
+            .collect::<Vec<_>>(),
+        );
 
-
-    let backup_file = format!("{}{}_backup.sql", chrono::offset::Local::now().format("%Y%m%d_%H%M%S"), remote_db);
-
-
-
-
-    // 1. Backup staging daatabase
-    let mut pg_dump = Command::new("pg_dump")
-    .arg("-h")
-    .arg(remote_host)
-    .arg("-p")
-    .arg(remote_port)
-    .arg("-U")
-    .arg(remote_username)
-    .arg("-W") // force password
-    .arg(remote_db)
-    .arg("-f") // output filename
-    .arg(&backup_file)
-    .spawn()
-    .expect("pg_dump failed to start");
-
-    let _ = pg_dump.wait().expect("pg_dump failed to run");
+        let seed = SubCommand::with_name("seed")
+        .about("Backup a seed database")
+        .args(
+            &[
+                Arg::new("output_file_location")
+                    .short('o')
+                    .long("output-file-location")
+                    .value_name("OUTPUT_FILE_LOCATION")
+                    .help("Location to search for the backup file")
+                    .default_value("."),
+            ]
+            .iter()
+            .chain(common_args.iter())
+            .cloned()
+            .collect::<Vec<_>>(),
+        );
     
+
+    let matches = app.subcommand(dump).subcommand(seed).get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("dump") {
+        println!("Dumping database...");
+        let host = matches.value_of("host").unwrap();
+        let port = matches.value_of("port").unwrap();
+        let username = matches.value_of("username").unwrap();
+        let database = matches.value_of("database").unwrap();
+        let output_file_location = matches.value_of("output_file_location").unwrap();
+
+        let backup_file = format!(
+            "{}/{}{}_backup.sql",
+            output_file_location,
+            chrono::offset::Local::now().format("%Y%m%d_%H%M%S"),
+            database
+        );
+
+        println!("Saving backup to: {}", backup_file);
+        let mut pg_dump = Command::new("pg_dump")
+            .arg("-h")
+            .arg(host)
+            .arg("-p")
+            .arg(port)
+            .arg("-U")
+            .arg(username)
+            .arg("-W") // force password
+            .arg(database)
+            .arg("-f") // output filename
+            .arg(&backup_file)
+            .spawn()
+            .expect("pg_dump failed to start");
+
+        let _ = pg_dump.wait().expect("pg_dump failed to run");
+
+        println!("Database dumped successfully");
+
+    } else if let Some(matches) = matches.subcommand_matches("seed") {
+        println!("Seeding database...");
+        let _host = matches.value_of("host").unwrap();
+        let _port = matches.value_of("port").unwrap();
+        let username = matches.value_of("username").unwrap();
+        let database = matches.value_of("database").unwrap();
+        let output_file_location = matches.value_of("output_file_location").unwrap_or(".");
+
+        let backup_dir = Path::new(output_file_location);
+        let mut latest_backup_file: Option<(String, std::time::SystemTime)> = None;
+
+        println!("Searching for backup file in: {}", backup_dir.display());
+        if let Ok(entries) = fs::read_dir(backup_dir) {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    let path = entry.path();
+                    if let Some(extension) = path.extension() {
+                        if extension == "sql" {
+                            if let Ok(metadata) = entry.metadata() {
+                                let modified = metadata.modified().unwrap_or(std::time::SystemTime::UNIX_EPOCH);
+                                if latest_backup_file
+                                    .as_ref()
+                                    .map(|(_, latest_modified)| modified > *latest_modified)
+                                    .unwrap_or(true)
+                                {
+                                    latest_backup_file = Some((path.to_string_lossy().into_owned(), modified));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if let Some((backup_file, _)) = latest_backup_file {
+            println!("Using backup file: {}", backup_file);
+
+            let file = std::fs::File::open(&backup_file).expect("Failed to open backup file");
+
+
+            let mut seed_db = Command::new("psql")
+            .arg("-U")
+            .arg(username)
+            .arg("-d")
+            .arg(database)
+            .stdin(std::process::Stdio::from(file))
+            .spawn()
+            .expect("seed_db failed to start");
+
+            let _ = seed_db.wait().expect("seed_db failed to run");
+            
+            println!("Database seeded successfully");
+        } else {
+            println!("No backup file found in the specified directory");
+        }
+    }
 }
